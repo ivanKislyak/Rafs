944b9374bd8f7ce7c97219eeaf7d9d04a3e491f0
IvanRPR <10yearmood@gmail.com>
2026-07-14 13:19:48 +0500

    The kislyak test application has been customised for Bootstrap

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def kislyak(request):
+    page_data = {
+        "heading": "Это Кисляк",
+        "description": "Первая страница Rafs, созданная через Django-шаблон.",
+    }
+
+    return render(request, "accounts/kislyak.html", page_data)
+
+
+def contact(request):
+    return HttpResponse("Контакты: +7 777 777 7777")
--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('kislyak', views.kislyak),
+    path('contact', views.contact),
 ]
+
