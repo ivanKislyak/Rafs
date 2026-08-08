bfd32dff3b1859a897fcddb5e6c628c0d9cb666b
kislyak <10yearmood@gmail.com>
2026-08-08 12:30:56 +0500

    feat: enhance User model with user_frames and user_lvl fields, update email field, and add migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,12 @@
+import math
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
-    email = models.EmailField(_("email address"), blank=True, null=True, unique=True, max_length=256)
+    user_lvl = models.IntegerField(default=1)
+    user_frames = models.IntegerField(default=1)
+    email = models.EmailField(_("email address"), null=True, unique=True, max_length=256)
     avatar_image = models.ImageField(upload_to="users/avatars/", blank=True, null=True)
     is_deleted = models.BooleanField(default=False)
 
@@ -11,3 +14,14 @@ class User(AbstractUser):
         self.is_deleted = True
         self.is_active = False
         self.save()
+
+    def save(self, *args, **kwargs):
+        if self.user_frames <= 0:
+            self.user_lvl = 1
+        else:
+            calculated_lvl = int(0.1 * math.sqrt(self.user_frames))
+            self.user_lvl = max(1, calculated_lvl)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.username} with email - {self.email} has {self.user_frames} frames and {self.user_lvl} lvl"
