b6c066bce64831c935714729a42745e3892a391a
kislyak <10yearmood@gmail.com>
2026-08-21 20:23:35 +0500

    feat: update name field in TypeOfWork, Genre, Country, Studio, and Person models to use JSONField

--- movies/movie_models.py
+++ movies/movie_models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class TypeOfWork(models.Model):
     wikidata_id = models.CharField(max_length=20, unique=True)
-    name = models.CharField(max_length=100)
+    name = models.JSONField(default=dict, blank=True)
 
     def __str__(self):
         return self.name
@@ -11,7 +11,7 @@ class TypeOfWork(models.Model):
 
 class Genre(models.Model):
     wikidata_id = models.CharField(max_length=20, unique=True)
-    name = models.CharField(max_length=100)
+    name = models.JSONField(default=dict, blank=True)
 
     def __str__(self):
         return self.name
@@ -19,7 +19,7 @@ class Genre(models.Model):
 
 class Country(models.Model):
     wikidata_id = models.CharField(max_length=20, unique=True)
-    name = models.CharField(max_length=100)
+    name = models.JSONField(default=dict, blank=True)
 
     def __str__(self):
         return self.name
@@ -27,7 +27,7 @@ class Country(models.Model):
 
 class Studio(models.Model):
     wikidata_id = models.CharField(max_length=20, unique=True)
-    name = models.CharField(max_length=100) 
+    name = models.JSONField(default=dict, blank=True) 
 
     def __str__(self):
         return self.name
@@ -35,7 +35,7 @@ class Studio(models.Model):
 
 class Person(models.Model):
     wikidata_id = models.CharField(max_length=20, unique=True)
-    name = models.CharField(max_length=200)
+    name = models.JSONField(default=dict, blank=True)
 
     def __str__(self):
         return self.name
