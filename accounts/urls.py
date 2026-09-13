3ef4456f8c7962571e605888388c157cef4b1a2e
kislyak <10yearmood@gmail.com>
2026-09-13 18:18:36 +0500

    fix: remove unused i18n path from accounts URLs and update LANGUAGE_CODE to 'en'

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile")
 ]
--- config/settings.py
+++ config/settings.py
@@ -153,7 +153,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 TIME_ZONE = 'Asia/Almaty'
 USE_I18N = True
 USE_TZ = True
