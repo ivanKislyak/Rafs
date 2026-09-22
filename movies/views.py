69ef96d874d6cfee60b30bd8a0c6ab6c8b6d75f0
kislyak <98189806+ivanKislyak@users.noreply.github.com>
2026-09-22 11:47:34 +0500

    Update views.py

--- movies/views.py
+++ movies/views.py
@@ -2,6 +2,7 @@ import json
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.translation import gettext as _
 
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
